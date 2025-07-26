ba28fb2cd09deb4c30b037fb6073075ca412e147
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2025-07-26 13:30:01 +0100

    Add Camunda boilerplate classes

---
+++ Example-4/leave-management/src/main/java/camunda_bpm/CamundaBpmProcessApplication.java
@@ -0,0 +1,9 @@
+package camunda_bpm;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication
+public class CamundaBpmProcessApplication extends ServletProcessApplication {
+
+}
